418f9891926b20e66e1bc934d77b98d2890b488f
yasuko goto <155627284+YasuGoto@users.noreply.github.com>
2025-09-28 00:58:36 +0900

    feat: パスワードのバリデーション作成 (#9)

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-import { initForm1 } from "./validation.ts";
+import { initForm1, initForm2 } from "./validation.ts";
 
 window.addEventListener("DOMContentLoaded", () => {
   initForm1("form1");
+  initForm2("form2");
 });
